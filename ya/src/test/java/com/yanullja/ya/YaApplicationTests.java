910e3055ad71f54f7c44bc17d37483dc12729e7d
sohee-ithub <im.sohee.1213@gmail.com>
2022-12-20 22:54:13 +0900

    spring boot base

---
+++ ya/src/test/java/com/yanullja/ya/YaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanullja.ya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
